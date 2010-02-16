72262a80a07be727a0e3a4597ac862cd240967e0
Ed Summers <ed@inkdroid.org>
2010-02-16 04:11:05 +0000

    work with relative feed urls

--- planet_config.py
+++ planet_config.py
@@ -7,6 +7,9 @@ Venus Planet configuration file.
 
 from socket import setdefaulttimeout
 from urllib import urlopen
+from urlparse import urlparse
+
+import logging
 
 from rdflib.graph import ConjunctiveGraph
 from rdflib.namespace import Namespace
@@ -16,13 +19,20 @@ setdefaulttimeout(10)
 w = Namespace('http://wiki.2010.dev8d.org/w/Special:URIResolver/Property-3A')
 
 def discover_feed(url):
+    logging.info("looking up feed url for %s" % url)
     try:
         html = urlopen(url).read()
         soup = BeautifulSoup(html)
         for link in soup.findAll('link', attrs={'rel': 'alternate'}):
-            return link['href']
+            feed_url = link['href']
+            # rewrite to feed to be absolute if its relative
+            if feed_url and not feed_url.startswith('http'):
+                u = urlparse(url)
+                feed_url = 'http://%s%s' % (u.netloc, feed_url)
+            return feed_url
     except Exception, e: 
-        pass
+        logging.error("error when performing feed discovery for %s: %s" % 
+                      (url, e))
 
 def blogs():
     g = ConjunctiveGraph('Sleepycat')
@@ -38,13 +48,13 @@ def print_config():
 """
 [Planet]
 
-name            = dev8d
-link            = http://inkdroid.org/dev8d 
+name            = planet-dev8d
+link            = http://inkdroid.org/planet-dev8d
 owner_name      = Ed Summers
 owner_email     = ehs@pobox.com
 output_theme    = theme
 cache_directory = cache
-output_dir      = output
+output_dir      = /var/www/inkdroid.org/planet-dev8d
 feed_timeout    = 20
 items_per_page  = 60
 log_level       = DEBUG
@@ -57,4 +67,8 @@ log_level       = DEBUG
             print "[%s]\nname = %s\n\n" % (feed, name)
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.info("generating planet config")
     print_config()
+    logging.info("finished generating planet config")
