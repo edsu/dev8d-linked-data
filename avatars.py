59c237d0fc44720905b722c67b65df72210dc312
Ed Summers <ehs@pobox.com>
2010-02-19 05:01:46 +0000

    added owl:sameAs link with dev8d user

--- avatars.py
+++ avatars.py
@@ -19,17 +19,19 @@ def attendee(graph, person):
     """
     for other_uri in graph.subjects(predicate=owl.sameAs, object=person):
         if 'dev8d.org' in other_uri:
-            return True
+            return other_uri
     return False
 
 def people(graph):
     for person, avatar in graph.subject_objects(predicate=foaf.img):
-        if not attendee(graph, person):
+        dev8d_uri = attendee(graph, person)
+        if not dev8d_uri:
             continue
+
         name = graph.value(subject=person, predicate=foaf.name)
         nick = graph.value(subject=person, predicate=foaf.nick)
         twitter = 'http://twitter.com/%s' % nick 
-        yield person, name, twitter, avatar
+        yield dev8d_uri, person, name, twitter, avatar
 
 def print_avatars_html(graph):
     print \
@@ -37,7 +39,8 @@ def print_avatars_html(graph):
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd"> 
 
 <html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:dcterms="http://purl.org/dc/terms/">
+      xmlns:foaf="http://xmlns.com/foaf/0.1/"
+      xmlns:owl="http://www.w3.org/2002/07/owl#">
     <head>
         <title>dev8d attendees</title>
         <style type="text/css">
@@ -57,16 +60,17 @@ def print_avatars_html(graph):
         </style>
     </head>
     <body>
-        <div id="banner"">
+        <div id="banner">
             <h1>dev8d attendees on twitter</h1>
             <a href="http://www.w3.org/2007/08/pyRdfa/extract?format=turtle&amp;uri=http://inkdroid.org/planet-dev8d/avatars/">
-                <img src="http://www.w3.org/Icons/SW/Buttons/sw-rdfa-gray.png" />
+                <img alt="rdfa" title="get turtle for this page" src="http://www.w3.org/Icons/SW/Buttons/sw-rdfa-gray.png" />
             </a>
+            <br />
+            <br />
         </div>
-       <br />
         <div id="avatars">
 """
-    for person, name, twitter, avatar in people(graph):
+    for dev8d_uri, person, name, twitter, avatar in people(graph):
         chunk = \
 """
 <span about="%s" typeof="foaf:Person">
@@ -74,27 +78,28 @@ def print_avatars_html(graph):
         <img resource="%s" rev="foaf:img" title="%s" 
              alt="%s" width="80" src="%s" />
     </a>
+    <span rel="owl:sameAs" resource="%s"></span>
 </span>
-""" % (person, twitter, person, name, name, avatar)
+""" % (person, twitter, person, name, name, avatar, dev8d_uri)
         print chunk.encode('utf-8')
 
     print \
 """
         <br />
         <br />
-        <div>
-            <p id="about">
-            This page is <a
-            href="http://github.com/edsu/dev8d-linked-data/blob/master/avatars.py">regenerated</a>
-            every two hours based on information stored in the <a
-            href="http://wiki.dev8d.org">dev8d semantic media wiki</a>
-            and social graph information in 
-            <a href="http://semantictweet.com">semantictweeet</a>. 
-            If you want to show up here add your twitter id to your 
-            user profile, see <a href="http://wiki.2010.dev8d.org/w/User:Dfflanders">dfflanders</a> 
-            profile for an example.
         </div>
+        <p id="about">
+        This page is <a
+        href="http://github.com/edsu/dev8d-linked-data/blob/master/avatars.py">regenerated</a>
+        every two hours based on information stored in the <a
+        href="http://wiki.dev8d.org">dev8d semantic media wiki</a>
+        and social graph information in 
+        <a href="http://semantictweet.com">semantictweeet</a>. 
+        If you want to show up here add your twitter id to your 
+        user profile, see <a href="http://wiki.2010.dev8d.org/w/User:Dfflanders">dfflanders</a> 
+        profile for an example.
         <br />
+        </p>
     </body>
 </html>
 """
